7e1255cb895349e7121627be33568b76994414ee
IVAN <49375725+na-613c@users.noreply.github.com>
2020-06-20 20:17:50 +0300

    Add files via upload

---
+++ precache-manifest.45eeec10015122223c32ec0764bbebef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6696fba53c815071fa33203d63495953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de5d758976c32f2c7c9b",
+    "url": "/static/css/main.7da0c2a8.chunk.css"
+  },
+  {
+    "revision": "faf73d456edbbbb8efb2",
+    "url": "/static/js/2.56e6133c.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.56e6133c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5d758976c32f2c7c9b",
+    "url": "/static/js/main.c2956c13.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
